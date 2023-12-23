427541ddd347d333bcd1bd0295d2349c9748e6cb
zharzi <zharzi@student.42lausanne.ch>
2023-12-23 01:58:08 +0100

    removing off-topic files

--- CPP/Training/GPT/Coplien/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "MaClasse.hpp"
-
-int main() {
-    MaClasse obj1; // Appel du constructeur par défaut
-    MaClasse obj3(obj1); // Appel du constructeur de copie
-    obj1 = obj3; // Appel de l'opérateur d'affectation
-    return 0;
-}
